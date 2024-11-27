75cbc19d2b9f35d30adfa0345947596397a5fd10
ngthvinhrai <johnnysteve123r@gmail.com>
2024-11-27 21:43:16 +0700

    add preprocessing

---
+++ preprocessing/bow/BagOfWord.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+class BagOfWord:
+    def __init__(self):
+        pass
+
+    def fit(self):
+        pass
+
+    def transform(self):
+        pass
---
+++ preprocessing/tfidf/tfdif.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+class TFIDF:
+    def __init__(self):
+        pass
+        
+    def fit(self):
+        pass
+
+    def transform(self):
+        pass
